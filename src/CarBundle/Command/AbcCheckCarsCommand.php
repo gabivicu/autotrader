fdc76f583dd080cb9932215d662dbdab4e3ce86d
gabivicu <gabi.vicu@gmail.com>
2021-05-23 09:43:24 +0300

    Console command as a service and display progress bar

--- src/CarBundle/Command/AbcCheckCarsCommand.php
+++ src/CarBundle/Command/AbcCheckCarsCommand.php
@@ -2,33 +2,62 @@
 
 namespace CarBundle\Command;
 
+use CarBundle\Service\DataChecker;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AbcCheckCarsCommand extends ContainerAwareCommand
+class AbcCheckCarsCommand extends Command
 {
+    /** @var DataChecker */
+    protected $carChecker;
+    /** @var EntityManager */
+    protected $manager;
+
+    /**
+     * AbcCheckCarsCommand constructor.
+     * @param DataChecker $carChecker
+     * @param EntityManager $manager
+     */
+    public function __construct(DataChecker $carChecker, EntityManager $manager)
+    {
+        $this->carChecker = $carChecker;
+        $this->manager = $manager;
+        parent::__construct();
+    }
+
+
     protected function configure()
     {
         $this
             ->setName('abc:check-cars')
             ->setDescription('Test command')
-            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('format', InputArgument::OPTIONAL, 'Progress format')
             ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $argument = $input->getArgument('argument');
+        $carRepository = $this->manager->getRepository('CarBundle:Car');
+        $cars = $carRepository->findAll();
+        $bar = new ProgressBar($output, count($cars));
 
-        if ($input->getOption('option')) {
-            // ...
+        $argument = $input->getArgument('format');
+        $bar->setFormat($argument);
+        $bar->start();
+        foreach($cars as $car){
+            $this->carChecker->checkCar($car);
+            sleep(1);
+            $bar->advance();
         }
 
-        $output->writeln('Command result.');
+        $bar->finish();
     }
 
 }
